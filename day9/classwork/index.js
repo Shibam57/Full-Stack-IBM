f16a0148c53aba09100e5bc92c6af49998eda3d7
Shibam57 <shibamsamantascience@gmail.com>
2025-02-24 00:32:33 +0530

    diugu

---
+++ day9/classwork/index.js
@@ -0,0 +1,25 @@
+// let a=10,b="10";
+// let c=a+-b;
+
+// let c=-a+-b;
+// let c=+a
+// let c=+a+b
+// console.log(typeof(c));
+// console.log(c);
+
+// let str="   ";
+// console.log(str.length);
+
+
+let a=10,b="20";
+
+const myFunction=(a,b)=>{
+    console.log(a+b);
+}
+myFunction(a,b)
+
+const hsd=(()=>{
+    console.log("sdh")
+})
+console.log(hsd());
+// what is hoyest string?
