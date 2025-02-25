f302490990c897b11d7a2bf3b454cb532bccafb8
Shibam57 <shibamsamantascience@gmail.com>
2025-02-25 11:35:56 +0530

    efhiew

--- day10/classwork/index.js
+++ day10/classwork/index.js
@@ -1,18 +1,78 @@
-let str="dddddddddddd dssss"
+// let str="dddddddddddd dssss"
 
-console.log(str.length);
+// console.log(str.length);
 
-console.log(str[str.length-1]);
+// console.log(str[str.length-1]);
 
+// const obj={
+//     firstName: {name:"John", subject:[566,515,215]},
+//     lastName: "Doe",
+//     age: 50,
+//     eyeColor: "blue"
+// }
+
+// delete obj.firstName;
+// console.log(obj);
+
+// console.log(object)            //clg then enter
+
+// let str="SV University";
+
+// console.log(str.length)
+
+// console.log(str[str.length-1])
+
+// str[0]="R"
+// console.log(str)
+
+// let arr=[1,2,3,"Shibam",1.2,true,false,null,undefined];
+// console.log(arr[3])
+// arr[3]="Jit";
+// console.log(arr)
+// console.log(arr[arr.length-1]);
+
+// arr.push("Ranit")
+// console.log(arr)
+// arr[4]="Ranit";
+// console.log(arr)
+
+// arr.pop()
+// console.log(arr)
+
+// //object
+
+// //Object Literal
 const obj={
-    firstName: {name:"John", subject:[566,515,215]},
-    lastName: "Doe",
-    age: 50,
-    eyeColor: "blue"
+    Subhajit:{"class":"B.Tech","Year":3,"Roll":184,"Shibam":["DBMS","CN","NM","DM","FS"]},
+    Raju:{"class":"B.Tech","Year":3,"Roll":104,"Shibam":["DBMS","CN","NM","FS"]},
+    1:25
 }
+console.log(obj)
 
-delete obj.firstName;
-console.log(obj);
+console.log(obj.Shibam.Shibam[0])
+obj["1"]="Jit";
+console.log(obj)
+delete obj["1"]
+console.log(obj)
 
-console.log(object)            //clg then enter
 
+delete obj["Shibam"]
+console.log(obj)
+
+obj.Virat={"class":"BCC1","Year":3,"Roll":104,"Shibam":["DBMS","CN","NM","FS"]}
+console.log(obj)
+
+obj.Virat={"class":"ICC","Year":3,"Roll":104,"Shibam":["DBMS","CN","NM","FS"]}
+console.log(obj)
+
+
+
+let name="Preet";
+let Class="BCA"
+let sub="FS"
+let newObj={name,Class,sub}
+console.log(newObj)
+
+
+//new key word
+//Constructor
